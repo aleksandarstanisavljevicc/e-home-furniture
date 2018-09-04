03578f6141b3c02e060c065f762a5cef2e830892
aleksandarstanisavljevicc <aleksandar.stanisavljevic@hotmail.com>
2018-09-04 20:36:48 +0200

    Adding connection string, setting models and authentication.

--- e_home_furniture/e_home_furniture/Startup.cs
+++ e_home_furniture/e_home_furniture/Startup.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Owin;
+﻿using e_home_furniture.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
 using Owin;
 
 [assembly: OwinStartupAttribute(typeof(e_home_furniture.Startup))]
@@ -9,6 +12,77 @@ namespace e_home_furniture
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            CreateRolesandUsers();
+        }
+
+
+        // In this method we will create default User roles and Admin user for login
+        private void CreateRolesandUsers()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+
+            // In Startup iam creating first Admin Role and creating a default Admin User 
+            if (!roleManager.RoleExists("Admin"))
+            {
+
+                // first we create Admin role
+                var role = new IdentityRole
+                {
+                    Name = RoleName.Admin
+                };
+                roleManager.Create(role);
+
+                //Here we create a Admin super user who will maintain the website				
+
+                var user = new ApplicationUser
+                {
+                    UserName = "panonit",
+                    Email = "panonitweb@gmail.com"
+                };
+
+                string userPWD = "Web.123";
+
+                var chkUser = UserManager.Create(user, userPWD);
+
+                //Add default User to Role Admin
+                if (chkUser.Succeeded)
+                {
+                    var result1 = UserManager.AddToRole(user.Id, "Admin");
+
+                }
+            }
+
+            // creating Creating Manager role 
+            /*if (!roleManager.RoleExists("Manager"))
+			{
+				var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+				role.Name = RoleName.Manager;
+				roleManager.Create(role);
+
+			}*/
+
+            // creating Creating Employee role 
+            else if (!roleManager.RoleExists("Employee"))
+            {
+                var role = new IdentityRole
+                {
+                    Name = RoleName.User
+                };
+                roleManager.Create(role);
+
+            }
+            else
+            {
+                var role = new IdentityRole
+                {
+                    Name = RoleName.User
+                };
+                roleManager.Create(role);
+            }
         }
     }
 }
