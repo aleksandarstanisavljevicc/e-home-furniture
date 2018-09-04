63ccadd62ce44bbc083bb02fd02fe063bd4a5dfb
aleksandarstanisavljevicc <aleksandar.stanisavljevic@hotmail.com>
2018-09-04 19:59:24 +0200

    Creating new project
    I was create new asp .net mvc project

---
+++ e_home_furniture/e_home_furniture/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="e_home_furniture.MvcApplication" Language="C#" %>
---
+++ e_home_furniture/e_home_furniture/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(e_home_furniture.Startup))]
+namespace e_home_furniture
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
